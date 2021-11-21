0bdd279957739067905fe0f1a5eb84f314613623
KSAJLAR <chowyuanyu12345@gmail.com>
2021-11-21 13:41:43 +0800

    added the CBT_Game page

---
+++ app/src/main/java/com/example/rewire/CBT_Game.java
@@ -0,0 +1,14 @@
+package com.example.rewire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CBT_Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cbt_game);
+    }
+}
