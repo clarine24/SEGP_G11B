ff7d1f34ea18efaecf5e0ccc2104a78f1d038fba
morhaf99 <morhaflababidi@outlook.com>
2021-12-08 18:56:51 +0400

    cbt_qa added

---
+++ app/src/main/java/com/example/rewire/cbt_qa.java
@@ -0,0 +1,14 @@
+package com.example.rewire;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class cbt_qa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cbt_question_answer);
+    }
+}
