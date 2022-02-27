3d863e0275949613574b6d8a1a7f7264f9c78b70
KSAJLAR <93910569+KSAJLAR@users.noreply.github.com>
2022-02-27 23:03:12 +0800

    educational page java file
    added the java file for educational page

---
+++ app/src/main/java/com/example/rewire/in_game_educationalpage.java
@@ -0,0 +1,14 @@
+package com.example.rewire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class in_game_educationalpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_game_educationalpage);
+    }
+}
