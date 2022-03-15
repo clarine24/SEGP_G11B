a7eb1213ce48cb7bbc0bb5b733e67aec05eda859
amsyarzero <amsyarminer555@gmail.com>
2022-03-15 20:48:48 +0800

    Connect main level menu to other sublevel menus

--- app/src/main/java/com/example/rewire/CBT_Game.java
+++ app/src/main/java/com/example/rewire/CBT_Game.java
@@ -15,7 +15,13 @@ public class CBT_Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cbt_game);
 
+        // buttons for each level
         final Button level1_btn = findViewById(R.id.level1);
+        final Button level2_btn = findViewById(R.id.level2);
+        final Button level3_btn = findViewById(R.id.level3);
+        final Button level4_btn = findViewById(R.id.level4);
+        final Button level5_btn = findViewById(R.id.level5);
+
         final ImageView hbg_menu = findViewById(R.id.hbg_menu);
         final ImageView hbg_menu_info = findViewById(R.id.hbg_menu_info);
         final ImageView hbg_menu_vol = findViewById(R.id.hbg_menu_vol);
@@ -23,6 +29,7 @@ public class CBT_Game extends AppCompatActivity {
         final ImageView hbg_menu_close = findViewById(R.id.hbg_menu_close);
         final ImageView home_btn = findViewById(R.id.home_icon);
 
+        // function for each button
         level1_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +37,39 @@ public class CBT_Game extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        level2_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CBT_Game.this, CBT_sublevel_2.class);
+                startActivity(intent);
+            }
+        });
+
+        level3_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CBT_Game.this, CBT_sublevel_3.class);
+                startActivity(intent);
+            }
+        });
+
+        level4_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CBT_Game.this, CBT_sublevel_4.class);
+                startActivity(intent);
+            }
+        });
+
+        level5_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CBT_Game.this, CBT_sublevel_5.class);
+                startActivity(intent);
+            }
+        });
+
 
         home_btn.setOnClickListener(new View.OnClickListener() {
             @Override
