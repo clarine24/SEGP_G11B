13f4f28663035d54420502d5606bb4f9cc099e57
alfsleeshih <shihalfslee@gmail.com>
2022-04-09 19:29:39 +0800

    implement delay on the color changes of the buttons on question and answer page

--- app/src/main/java/com/example/rewire/cbt_qa.java
+++ app/src/main/java/com/example/rewire/cbt_qa.java
@@ -44,46 +44,73 @@ public class cbt_qa extends AppCompatActivity {
         final Button optionA = findViewById(R.id.optionA);
         final Button optionB = findViewById(R.id.optionB);
         final Button optionC = findViewById(R.id.optionC);
-        final ImageView optionA_ans = findViewById(R.id.optionA_ans);
-        final ImageView optionB_ans = findViewById(R.id.optionB_ans);
-        final ImageView optionC_ans = findViewById(R.id.optionC_ans);
+        final ImageView optionA_icon = findViewById(R.id.optionA_icon);
+        final ImageView optionB_icon = findViewById(R.id.optionB_icon);
+        final ImageView optionC_icon = findViewById(R.id.optionC_icon);
 
         //assume OPTION A is the correct answer
         char correct = 'A';
 
-        optionA.setOnClickListener(v -> {
-            optionA_ans.setVisibility(View.VISIBLE);
+        optionA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (correct == 'A') {
+                    showCorrectAnswer(optionA, optionA_icon);
+                }
+                else {
+                    showIncorrectAnswer(optionA, optionA_icon);
+                }
+            }
 
-            if (correct == 'A')
-                correctAns(optionA);
-            else
-                wrongAns(optionA);
         });
 
-        optionB.setOnClickListener(v -> {
-            optionB_ans.setVisibility(View.VISIBLE);
-
-            if (correct == 'B')
-                correctAns(optionB);
-            else
-                wrongAns(optionB);
+        optionB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (correct == 'B')
+                    showCorrectAnswer(optionB, optionB_icon);
+                else
+                    showIncorrectAnswer(optionB, optionB_icon);
+            }
         });
 
-        optionC.setOnClickListener(v -> {
-            optionC_ans.setVisibility(View.VISIBLE);
-
-            if (correct == 'C')
-                correctAns(optionC);
-            else
-                wrongAns(optionC);
+        optionC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (correct == 'C')
+                    showCorrectAnswer(optionC, optionC_icon);
+                else
+                    showIncorrectAnswer(optionC, optionC_icon);
+            }
         });
+
     }
 
-    private void correctAns(Button option) {
+    private void showCorrectAnswer(Button option, ImageView icon) {
+        option.setClickable(false);
+        icon.setVisibility(View.VISIBLE);
         option.setBackgroundColor(getResources().getColor(R.color.dark_green));
+        option.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                option.setClickable(true);
+                option.setBackgroundColor(getResources().getColor(R.color.dark_pink));
+                icon.setVisibility(View.INVISIBLE);
+            }
+        }, 2000);
     }
 
-    private void wrongAns(Button option) {
+    private void showIncorrectAnswer(Button option, ImageView icon) {
+        option.setClickable(false);
+        icon.setVisibility(View.VISIBLE);
         option.setBackgroundColor(getResources().getColor(R.color.dark_red));
+        option.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                option.setClickable(true);
+                option.setBackgroundColor(getResources().getColor(R.color.dark_pink));
+                icon.setVisibility(View.INVISIBLE);
+            }
+        }, 2000);
     }
 }
